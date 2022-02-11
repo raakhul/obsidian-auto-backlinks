d4868963156da2b2396728e4040388097cac3920
raakhul <91585121+raakhul@users.noreply.github.com>
2022-02-11 20:17:24 +0530

    Code changes for working auto backlinks

--- main.ts
+++ main.ts
@@ -1,137 +1,88 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {MarkdownView,Plugin, TFile} from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class AutomaticLinks extends Plugin {
 
 	async onload() {
-		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		this.addRibbonIcon("links-going-out", "Backlinks", async () => {    
+
+			const editor = this.app.workspace.getActiveViewOfType(MarkdownView)?.editor;
+			const noteFile = this.app.workspace.getActiveFile(); // Currently Open Note
+			if(!noteFile.name) return; // Nothing Open
+
+			const filenamesArray=await this.extractFilenameSameLevel(noteFile);
+
+			const selectedText=editor.getSelection();
+			editor.replaceSelection(filenamesArray.toString(),selectedText);
+
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
 
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	//To Read the filenames under same level
+	async extractFilenameSameLevel(noteFile: TFile):  Promise<any[]> {
+	
+		// Read the currently open note file. We are reading it off the HDD - we are NOT accessing the editor to do this.
+		let currentName = noteFile.name; //Active File Name
+		let currentPath = noteFile.path; //Path of Active File
+		let activePath = currentPath;
+		let currentPathSlash = currentPath.match(/\//gm).length; //Caculating no of slash from path of active file 
+		let finalFilePath = new Array();
+		let filePath=this.app.vault.getMarkdownFiles().map((file) => file.path) //List of all markdown files
+
+		currentPath = currentPath.replace(currentName,"");
+		currentPath = currentPath.replace("/","\/"); //Extracting the path before Active file
+
+		var regex = new RegExp(currentPath+".*"); //Generating regex from variable
+
+		//Extracting the filepath which comes under same level based on active file and no of /
+		for (var i=0; i<filePath.length; i++) {
+			if (regex.test(filePath[i])) {
+				if ((currentPathSlash==filePath[i].match(/\//gm).length) && (activePath!==filePath[i])) {
+					finalFilePath.push(filePath[i].toString())
+				}
+			}
+		}
+
+		let filenamesLinkArray=this.generateFilenamesLink(finalFilePath); //Generate md link for filenames under same path
+
+		return filenamesLinkArray;
 	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
+
+	//Generate md link for filenames under same path
+	generateFilenamesLink (filenames: any[]): any[] {
+
+		let filenamesArray: Array<String>;
+		let filenamesLinkArray = new Array();
+		filenamesArray = filenames.toString().split(",");
+		
+
+		for (var i=0; i<filenamesArray.length;i++) {
+			let filename: String;
+
+			//Selecting the other filenames from file path
+			let matchedPattern=(filenamesArray[i].match(/[\w-\s]+\.../g)).toString(); 
+			matchedPattern=matchedPattern.replace(".md",""); //Removing .md extension
+
+			//Constructing md links for filenames under same level
+			if (i==0) {
+				filename="[".concat(matchedPattern.toString())
+				filename=filename.concat("](").concat(filenamesArray[i].toString().replace(/\s/g,"%20")).concat(")");
+				filenamesLinkArray.push(filename.toString());
+			}
+			else {
+				filename=" [".concat(matchedPattern.toString()).concat("](").concat(filenamesArray[i].toString().replace(/\s/g,"%20")).concat(")");
+				filenamesLinkArray.push(filename.toString());
+			}
+		}
+
+		filenamesLinkArray=filenamesLinkArray.sort();
+
+		return filenamesLinkArray;
 	}
-}
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
 }
